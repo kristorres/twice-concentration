9fafc068459e07a337bd1a8c68a17d94a84a0871
Kris Torres <kristorresdayo@gmail.com>
2021-02-23 16:30:37 -0800

    Create a simple backdrop

---
+++ src/app.svelte
@@ -0,0 +1,19 @@
+<style>
+    .layout {
+        display: flex;
+        flex-direction: column;
+        background-color: #CFD8DC;
+        min-height: 100vh;
+    }
+
+    @media (prefers-color-scheme: dark) {
+        .layout {
+            background-color: #263238;
+        }
+    }
+</style>
+
+<root>
+    <div class="layout">
+    </div>
+</root>
