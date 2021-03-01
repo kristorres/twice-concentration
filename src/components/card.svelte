9fcfa685c4c3a0e15a2ed8fd4fdeb42841f63317
Kris Torres <kristorresdayo@gmail.com>
2021-03-01 12:09:24 -0800

    Implement the Card component

---
+++ src/components/card.svelte
@@ -0,0 +1,35 @@
+<script>
+    export let card;
+    export let disabled;
+    export let index;
+
+    $: background = card.isFaceUp
+        ? `background-image: url("${card.url}")`
+        : "background: radial-gradient(#1A64D7, #003BA5)";
+</script>
+
+<style>
+    .card {
+        background-size: cover;
+        border-radius: 8px;
+        box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.1);
+        cursor: pointer;
+    }
+    .disabled {
+        pointer-events: none;
+    }
+    .matched {
+        opacity: 0.75;
+        box-shadow: none;
+    }
+</style>
+
+<div
+    data-id={index}
+    class="card"
+    class:disabled={disabled}
+    class:matched={card.isMatched}
+    style={background}
+    on:click
+>
+</div>
